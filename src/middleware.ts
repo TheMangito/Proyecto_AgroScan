7feb5d125f52d64a594aafc0f2a44e08967f3948
TheMangito <themanguito21@gmail.com>
2025-05-12 22:46:56 -0600

    fix middleware para deploy en vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,18 @@
+// src/middleware.ts
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
+const isProtected = createRouteMatcher(["/dashboard(.*)"]);
 
+export const onRequest = clerkMiddleware(
+  (auth, context) => {
+    const { userId, redirectToSignIn } = auth();
+
+    if (!userId && isProtected(context.request)) {
+      return redirectToSignIn();
+    }
+
+  },
+  {
+    // Aquí van opciones válidas
+  }
+);
